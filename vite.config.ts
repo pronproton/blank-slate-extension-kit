956504ea15ff321495323823beb242fa1fb245db
pronproton <pronproton@tutamail.com>
2025-06-04 14:21:27 +0000

    Добавляю vite.config.ts через API

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        popup: path.resolve(__dirname, 'popup.html')
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
+      }
+    },
+    outDir: 'dist',
+    assetsDir: '',
+  },
+  define: {
+    global: 'globalThis',
+  },
+  publicDir: 'public',
+}));
